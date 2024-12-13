f3654ca8f3fbe2e029340da77959ccd8ae97a2ff
Hafiz M. Yusuf <hafizmasoud7@gmail.com>
2024-12-13 02:07:42 +0000

    auth

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
 import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
-import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/authMiddleware';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 import { APIError, errorResponse } from '../middlewares/error';
 
 /**
